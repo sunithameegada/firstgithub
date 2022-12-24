7d49f26d50390ea2c344798debada1b2d446f11c
Keerthi Reddy <you@example.com>
2022-12-24 14:35:19 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def suni(request):
+  return HttpResponse('<h1><marquee>SHIVA IS ALL AND EVERYTHING.HE IS THE UNIVERSE LOVE U</h1></marquee>')
